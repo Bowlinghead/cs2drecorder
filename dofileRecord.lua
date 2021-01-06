39b5ed97271a41ca00fa878d8aecb029fbd50dfd
Bowlinghead <timpape97@web.de>
2021-01-06 21:54:03 +0100

    Add files via upload

--- dofileRecord.lua
+++ dofileRecord.lua
@@ -2,23 +2,26 @@
 	If you want to record a match then ONLY load this file
 	
 ]]
+
+
+
+
+
+
+
+
+
+
+
+
 pathRoot = "sys/lua/GameReplay/";
 path = pathRoot.."Recorder/";
-
 
 
 dofile(pathRoot.."config.lua")
 
 
-msg("hehe1")
+dofile(path.."Assembler.lua")
 dofile(path.."GameState.lua");
-msg("hehe2")
-
 dofile(path.."GameReplay.lua");
-msg("hehe3")
-
-dofile(path.."Assembler.lua");
-msg("hehe4")
-
 dofile(path.."Addhooks.lua");
-msg("hehe5")
